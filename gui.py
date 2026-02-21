c50712a5614e205345eddc1433d9547731365408
osmanx522 <osmanfadil52@gmail.com>
2026-02-21 21:43:04 +0300

    the glitch effect button is working

--- gui.py
+++ gui.py
@@ -3,6 +3,7 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
 from io import BytesIO
 from resim_islemleri import ResimMotoru
+
 class AnaPencere(QWidget):
     def __init__(self):
         super().__init__()
@@ -21,11 +22,12 @@ class AnaPencere(QWidget):
         btn_ac = QPushButton("Resim Aç")
         btn_ac.clicked.connect(self.resim_yukle)
         btn_kaydet = QPushButton("Resim Kaydet")
-        btn_retro = QPushButton("Retro")
+        btn_gltich = QPushButton("Glitch Efekti")
+        btn_gltich.clicked.connect(self.glitch)
         btn_bulanik = QPushButton("Bulanik")
         izgara.addWidget(btn_ac, 0,0)
         izgara.addWidget(btn_kaydet, 0,1)
-        izgara.addWidget(btn_retro, 1,0)
+        izgara.addWidget(btn_gltich, 1,0)
         izgara.addWidget(btn_bulanik, 1,1)
         sag_duzen.addLayout(izgara)
 
@@ -36,19 +38,22 @@ class AnaPencere(QWidget):
         Hlayout.addLayout(sag_duzen)
         self.setLayout(Hlayout)
 
+    def resim_guncelleme(self, resim):
+        hafiza = BytesIO()
+        resim.save(hafiza, format="BMP")
+        pixmap_resim = QPixmap()
+        pixmap_resim.loadFromData(hafiza.getvalue())
+        # Qt.KeepAspectRatio: Resmin en-boy oranını korur
+        # Qt.SmoothTransformation: Küçültürken piksellenmeyi önler
+        duzenli_resim = pixmap_resim.scaled(800, 600, Qt.KeepAspectRatio, Qt.SmoothTransformation)
+        self.resim_alani.setPixmap(duzenli_resim) # Resim alanına resmi yerleştirir.
+
     def resim_yukle(self):
         dosya_yolu, _ = QFileDialog.getOpenFileName(self, "Bir Resim Seç", "", "Resimler (*.png *.jpg *.jpeg)")
         if dosya_yolu:
             pil_resmi = self.motor.resim_yukle(dosya_yolu)
-
-            hafiza = BytesIO()
-            pil_resmi.save(hafiza, format="PNG")
-            
-            pixmap_resim = QPixmap()
-            pixmap_resim.loadFromData(hafiza.getvalue())
-
-            # Qt.KeepAspectRatio: Resmin en-boy oranını korur
-            # Qt.SmoothTransformation: Küçültürken piksellenmeyi önler
-            duzenli_resim = pixmap_resim.scaled(800, 600, Qt.KeepAspectRatio, Qt.SmoothTransformation)
-
-            self.resim_alani.setPixmap(duzenli_resim) # Resim alanına resmi yerleştirir.
+            self.resim_guncelleme(pil_resmi)
+    
+    def glitch(self):
+        retro_resim = self.motor.glitch_effekti()
+        self.resim_guncelleme(retro_resim)
--- resim_islemleri.py
+++ resim_islemleri.py
@@ -5,9 +5,20 @@ class ResimMotoru:
     def __init__(self):
         self.orijinal_resim = None
         self.guncel_resim = None
-    
+        self.orijinal_resim_array = None
+        self.guncel_resim_array = None
+
     def resim_yukle(self, resim_yolu):
         self.orijinal_resim = Image.open(resim_yolu)
         self.guncel_resim = self.orijinal_resim.copy()
-
+        self.orijinal_resim_array = np.array(self.orijinal_resim)
+        self.guncel_resim_array = self.orijinal_resim_array.copy()
+        return self.guncel_resim
+    
+    def glitch_effekti(self):
+        r, g, b = self.guncel_resim_array[:,:,0] , self.guncel_resim_array[:,:,1], self.guncel_resim_array[:,:,2]
+        r_shift = np.roll(r, 10, axis=1)
+        b_shift = np.roll(b, -10, axis=1)
+        self.guncel_resim_array = np.dstack((r_shift, g, b_shift))
+        self.guncel_resim = Image.fromarray(self.guncel_resim_array)
         return self.guncel_resim
