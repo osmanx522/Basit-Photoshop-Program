71e7c902860a6e0cddeeae924a20e2bdc75c4818
osmanx522 <osmanfadil52@gmail.com>
2026-03-03 18:07:54 +0300

    The code was optimized with Gemini.

--- button_function.py
+++ button_function.py
@@ -1,51 +1,58 @@
 from PyQt5.QtWidgets import QFileDialog
+
 class ButonYoneticisi():
     def __init__(self, pencere):
         self.pencere = pencere
-        self.motor= self.pencere.motor
+        self.motor = self.pencere.motor
 
     def efekt_uygula(self):
-        ''' Bu fonksiyon gui.py de bulunan ayara cubugundan gelen veriyi ve secili efekti
-            motora iletir ve uygular.'''
+        """Slider kaydırıldığında anlık (real-time) önizleme yapar."""
+        if self.motor.onizleme_resmi:
+            siddet = self.pencere.ayar_cubugu.value()
+            secili_efekt = self.pencere.efekt_grubu.checkedId()
+            
+            if secili_efekt in self.motor.effect_manager:
+                # Efekti SADECE önizleme resmi üzerinde hesapla (Hızlıdır)
+                sonuc = self.motor.effect_manager[secili_efekt](siddet, self.motor.onizleme_resmi)
+                self.pencere.resim_guncelleme(sonuc)
+
+    def uygula(self):
+        """'Uygula' butonuna basıldığında efekti orijinal yüksek çözünürlüklü resme işler."""
         if self.motor.orijinal_resim:
             siddet = self.pencere.ayar_cubugu.value()
             secili_efekt = self.pencere.efekt_grubu.checkedId()
+            
             if secili_efekt in self.motor.effect_manager:
-                resim = self.motor.effect_manager[secili_efekt](siddet)
-                self.pencere.resim_guncelleme(resim)
+                # 1. Efekti orijinal büyük resme uygula (Kalıcı hale getir)
+                self.motor.orijinal_resim = self.motor.effect_manager[secili_efekt](siddet, self.motor.orijinal_resim)
+                
+                # 2. Yeni halinden yeni bir önizleme katmanı oluştur (Bir sonraki efekt için)
+                self.motor.onizleme_resmi = self.motor.orijinal_resim.copy()
+                self.motor.onizleme_resmi.thumbnail((800, 600))
+                
+                # 3. Arayüzü güncelle
+                self.pencere.resim_guncelleme(self.motor.onizleme_resmi)
+                self.pencere.ayar_cubugu.setValue(50) # Slider'ı nötr noktaya çek
 
-    # DOSYA İSLEMLERİ
     def yukle(self):
-        '''Dosyayı yükleme penceresini açar ve motora dosya yolunu iletir.
-            Motordan dönene dosyayı ekrana gösterir ve bilgilerini yazdırır.'''
         dosya_yolu, _ = QFileDialog.getOpenFileName(self.pencere, "Bir Resim Seç", "", "Resimler (*.png *.jpg *.jpeg)")
         if dosya_yolu:
-            pil_resmi = self.motor.resim_yukle(dosya_yolu)
+            onizleme = self.motor.resim_yukle(dosya_yolu)
             bilgiler = self.motor.resim_bilgiler()
-            self.pencere.resim_guncelleme(pil_resmi)
-            self.pencere.bilgi_etiketi.setText(f"Çözünürlük: {bilgiler['resolution'][0]}x{bilgiler['resolution'][1]} Format: {bilgiler['format']} Mod: {bilgiler['mode']}")
+            self.pencere.resim_guncelleme(onizleme)
+            self.pencere.bilgi_etiketi.setText(f"Çözünürlük: {bilgiler['resolution'][0]}x{bilgiler['resolution'][1]} | Format: {bilgiler['format']}")
 
     def kaydet(self):
-        '''
-        Dosyayı Kaydetmet için kaydetme penceresini oluşturur.
-        '''
         if self.motor.orijinal_resim:
-            dosya_yolu, _ = QFileDialog.getSaveFileName(self.pencere, "Kaydedilcek Yolu Seçin", "","PNG Formatı (*.png);;JPEG Formatı (*.jpg *.jpeg)")
+            dosya_yolu, _ = QFileDialog.getSaveFileName(self.pencere, "Kaydedilecek Yolu Seç", "", "PNG (*.png);;JPEG (*.jpg)")
             if dosya_yolu:
                 self.motor.resim_kaydet(dosya_yolu)
-    def uygula(self):
-        '''
-        Uygula butonu için yapılmıştır.
-        Yapılan ayarları motordaki orijinal resime eşitler.
-        '''
-        if self.motor.orijinal_resim:
-            self.motor.orijinal_resim = self.motor.guncel_resim.copy()
-            self.pencere.ayar_cubugu.setValue(0)
 
     def sifirla(self):
-        '''Yapılan bütün efektleri sıfırlar.'''
-        if self.motor.orijinal_resim:
+        """Her şeyi en baştaki orijinal haline döndürür."""
+        if self.motor.ilk_resim:
             self.motor.orijinal_resim = self.motor.ilk_resim.copy()
-            self.motor.guncel_resim = self.motor.ilk_resim.copy()
-            self.pencere.ayar_cubugu.setValue(0)
-            self.pencere.resim_guncelleme(self.motor.orijinal_resim)
+            self.motor.onizleme_resmi = self.motor.ilk_resim.copy()
+            self.motor.onizleme_resmi.thumbnail((800, 600))
+            self.pencere.ayar_cubugu.setValue(50)
+            self.pencere.resim_guncelleme(self.motor.onizleme_resmi)
--- resim_motoru.py
+++ resim_motoru.py
@@ -3,56 +3,58 @@ from PIL import Image, ImageFilter, ImageEnhance
 
 class ResimMotoru:
     def __init__(self):
-        self.ilk_resim = None
-        self.orijinal_resim = None 
-        self.guncel_resim = None
-        # Efekt Manager ana efekt grubunu seçmemizi sağlar
+        self.ilk_resim = None        # Dosyadan ilk açılan ham hali (Geri dönüş için)
+        self.orijinal_resim = None   # Efektlerin kalıcı olarak bindiği yüksek çözünürlüklü hali
+        self.onizleme_resmi = None   # Sadece ekranda hızlı görmek için kullanılan küçük resim
+        
+        # Efekt Manager: Fonksiyonları merkezi bir yerden yönetir
         self.effect_manager = {
             1: self.glitch_efekti,
             2: self.blur_efekti,
-            3: lambda s: self.enhancements_apply(ImageEnhance.Color, s),
-            4: lambda s: self.enhancements_apply(ImageEnhance.Brightness, s),
-            5: lambda s: self.enhancements_apply(ImageEnhance.Contrast, s),
-            6: lambda s: self.enhancements_apply(ImageEnhance.Sharpness, s),
+            3: lambda s, res: self.enhancements_apply(ImageEnhance.Color, s, res),
+            4: lambda s, res: self.enhancements_apply(ImageEnhance.Brightness, s, res),
+            5: lambda s, res: self.enhancements_apply(ImageEnhance.Contrast, s, res),
+            6: lambda s, res: self.enhancements_apply(ImageEnhance.Sharpness, s, res),
         }
+
     def resim_yukle(self, resim_yolu):
-        '''BU Fonksiyon resmi arka planda yüklemeyi sağlar ve resmi döndürür.'''
         self.ilk_resim = Image.open(resim_yolu)
         self.orijinal_resim = self.ilk_resim.copy()
-        self.guncel_resim = self.ilk_resim.copy()
-        return self.guncel_resim
-    
+        
+        # Önizleme için küçük bir kopya oluştur (Performans için kritik)
+        self.onizleme_resmi = self.ilk_resim.copy()
+        self.onizleme_resmi.thumbnail((800, 600))
+        return self.onizleme_resmi
+
     def resim_bilgiler(self):
-        '''Bu Fonksiyon resmin bilgilerini tutar'''
-        dic = {
+        if not self.ilk_resim: return {}
+        return {
             "resolution": self.ilk_resim.size,
             "format": self.ilk_resim.format,
             "mode": self.ilk_resim.mode
         }
-        return dic
-    
-    def resim_kaydet(self, resim_yolu):
-        '''Bu fonksiyon resmi kaydeder sadece'''
-        if self.guncel_resim:
-            self.guncel_resim.save(resim_yolu)
 
-    def glitch_efekti(self, siddet):
-        '''Bu fonksiyon glitch efekti uygular'''
-        img_arr = np.array(self.orijinal_resim)
-        r, g, b = img_arr[:,:,0] ,img_arr[:,:,1], img_arr[:,:,2]
+    def resim_kaydet(self, resim_yolu):
+        if self.orijinal_resim:
+            self.orijinal_resim.save(resim_yolu)
+
+    # EFEKT FONKSİYONLARI
+    def glitch_efekti(self, siddet, kaynak_resim):
+        img_arr = np.array(kaynak_resim)
+        if len(img_arr.shape) < 3: return kaynak_resim # RGB değilse işlem yapma
+        
+        r, g, b = img_arr[:,:,0], img_arr[:,:,1], img_arr[:,:,2]
         r_shift = np.roll(r, siddet, axis=1)
         b_shift = np.roll(b, -siddet, axis=1)
+        
         img_arr_new = np.dstack((r_shift, g, b_shift))
-        self.guncel_resim = Image.fromarray(img_arr_new)
-        return self.guncel_resim
-    
-    def blur_efekti(self, siddet):
-        '''Bu fonksiyon blur efekti uygular'''
-        self.guncel_resim = self.orijinal_resim.filter(ImageFilter.GaussianBlur(siddet))
-        return self.guncel_resim
-    
-    def enhancements_apply(self, enh_tool, siddet):
-        """Resim İyileştirmeleri İçin Oluşturulmuştur"""
-        siddet = 1-(siddet/100)
-        self.guncel_resim = enh_tool(self.orijinal_resim).enhance(siddet)
-        return self.guncel_resim
+        return Image.fromarray(img_arr_new)
+
+    def blur_efekti(self, siddet, kaynak_resim):
+        # GaussianBlur 0-100 arası çok fazladır, değeri normalize ediyoruz
+        return kaynak_resim.filter(ImageFilter.GaussianBlur(siddet / 10))
+
+    def enhancements_apply(self, enh_tool, siddet, kaynak_resim):
+        # Orta nokta 50 (Değişim yok = 1.0). 0-100 arası 0.0 ile 2.0 arası çarpan verir.
+        faktor = siddet / 50 
+        return enh_tool(kaynak_resim).enhance(faktor)
